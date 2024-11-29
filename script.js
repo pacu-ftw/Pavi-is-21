2701a1a8840b037c6983edef909ddafa8ea12225
pacu-ftw <chatme454@gmail.com>
2024-11-29 18:16:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function() {
+    const pages = document.querySelectorAll('.page');
+    
+    pages.forEach(page => {
+        const pagePosition = page.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (pagePosition < windowHeight * 0.8) {
+            page.classList.add('visible');
+        }
+    });
+});
+
